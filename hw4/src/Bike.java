cc5974fff78ed08cce561982a8aa918d54ada50f
VitaliiSorokin <x3meast@gmail.com>
2017-10-03 12:15:03 +0300

    hw4 update

--- hw4/src/Bike.java
+++ hw4/src/Bike.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
-
 public class Bike {
+    protected int wheel;
+    protected int gears;
+    protected String breaks;
+    protected String size;
+    protected String color;
+
+    public void setWheel(int wheel) {
+        this.wheel = wheel;
+    }
+
+    public int getWheel() {
+        return wheel;
+    }
+
+    public void setGears(int gears) {
+        this.gears = gears;
+    }
+
+    public int getGears() {
+        return gears;
+    }
+
+    public void setBreaks(String breaks) {
+        this.breaks = breaks;
+    }
+
+    public String getBreaks() {
+        return breaks;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    final Class<? extends Bike> getBikeClass(Bike bikeObj) {
+        return bikeObj.getClass();
+    }
 }
--- hw4/src/Demo.java
+++ hw4/src/Demo.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class Demo {
+    public static void main(String[] args) {
+        Bike general = new Bike();
+        Cannondale five = new Cannondale();
+        Cube ltd = new Cube();
+
+        general.setWheel(23);
+        System.out.println(general.getWheel());
+
+        five.setWheel(24,36);
+        five.getWheel();
+
+        ltd.setColor("black");
+        ltd.setSize("M");
+        ltd.setBreaks("HYDRAULIC");
+
+        System.out.println(ltd.getColor() + ltd.getSize() + ltd.getBreaks());
+
+        System.out.println((ltd.getClass()));
+    }
 }
+
